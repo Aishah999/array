b082791cf47e23b85e13c5619083a88f65580243
Aishah999 <aishaah@code.edu.az>
2024-02-21 14:42:40 +0400

    first commit

---
+++ Array/Array/Array/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//int[] numbers= { 24, 67, 56, 78, 85 };
+//int sum = default;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//sum += numbers[i];
+//}
+//Console.WriteLine(sum);
+
+
+//int[] numbers = { 13, 26, 44, 59, 87, 99 };
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//i = numbers.Length - 1;
+//Console.WriteLine(numbers[i]);
+//}
+
+//int[] numbers = { 33, 55, 60, 42, 77, 85 };
+//for (int i = 0; i < numbers.Length; i++)
+//{
+    //i = numbers.Length - 1;
+    //Console.WriteLine(numbers[0] + numbers[i]);
+//}
